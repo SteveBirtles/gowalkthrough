21510cd5148b9febc16b8151d97dcddd177a3b95
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-11-23 16:15:43 +0000

    Some notes

--- server/main.go
+++ server/main.go
@@ -9,8 +9,24 @@ import (
 func main() {
 
 	http.HandleFunc("/client/", controllers.StaticContent)
+
 	http.HandleFunc("/console/list", controllers.ListConsoles)
 	http.HandleFunc("/game/list/", controllers.ListGames)
+
+	/*/console/list
+	/game/list/{id}
+	/accessory/list/{id}
+	/admin/login
+	/admin/check
+	/console/get/{id}
+	/console/save/{id}
+	/console/delete/{id}
+	/game/get/{id}
+	/game/save/{id}
+	/game/delete/{id}
+	/accessory/get/{id}
+	/accessory/save/{id}
+	/accessory/delete/{id}*/
 
 	err := http.ListenAndServe(":8081", http.DefaultServeMux)
 	if err != nil {
