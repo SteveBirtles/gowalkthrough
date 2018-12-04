bbfd60f1fdf23e3bfa2dd63d5f4b5313af679ce7
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-12-04 14:49:24 +0000

    Empty functions.

---
+++ server/controllers/accessory.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "net/http"
+
+func ListAccessories(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetAccessory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SaveAccessory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteAccessory(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ server/controllers/admin.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "net/http"
+
+func AdminLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AdminCheck(w http.ResponseWriter, r *http.Request) {
+
+}
